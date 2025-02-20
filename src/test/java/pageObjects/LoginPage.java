14182976d10635cafaf20d42bf09699ccf8d7b05
tohfademo <tohfademo0@gmail.com>
2025-02-20 07:58:21 -0600

    Implemented @TTLPH2-13 Validate login with invalid data

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -3,21 +3,28 @@ package pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utilities.ElementUtil;
+import utilities.ReadConfig;
 
 public class LoginPage {
 	
 	private WebDriver driver;
 	private ElementUtil util;
+	private ReadConfig readConfig;
 	
 	private By username= By.xpath("//input[@id='username']");
 	private By password=By.xpath("//input[@id='password']");
 	private By dropDownArraow = By.xpath("//span[text()='Select the role']/..//following-sibling::div[contains(@class,'arrow-wrapper')]");
 	private By loginbutton= By.xpath("//button[@id='login']");
 	
+	//Element not available yet. Temporary Xpath
+	private By errorMessage_InvalidCredentials=By.xpath("//*[text()='Invalid username and password Please try again']");
+	private By errorMessage_nullUsername=By.xpath("//mat-error[normalize-space()='Please enter your user name']");
+	private By errorMessage_nullPassword=By.xpath("//mat-error[normalize-space()='Please enter your password']");
 	
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 		util = new ElementUtil(this.driver);
+		readConfig = new ReadConfig();
 	}
 	
 	/**
@@ -43,50 +50,48 @@ public class LoginPage {
 	 * @param role
 	 * @return HomePage
 	 */
-	public HomePage doLoginWithValidCredentials(String username, String password, String role) {
+	public Object doLoginWithValidCredentials(String username, String password, String role, String... keyboardOrMouseClick) {
 		
+		Object obj = null;
+		
+		//Credentials
 		util.doSendKeys(this.username, username);
 		util.doSendKeys(this.password, password);
 
+		//Role
 		util.doClick(dropDownArraow);
 		By roleOption= By.xpath("//span[normalize-space()='"+role+"']");
 		util.doClick(roleOption);
-
-		util.doClick(loginbutton);
-		return new HomePage(driver);
-	}
-	
-	/**
-	 * This method performs valid login through using mouse or keyboard click on Login button
-	 * @param username
-	 * @param password
-	 * @param role
-	 * @param keyboardOrMouseClick
-	 * @return HomePage
-	 */
-	public HomePage doLoginWithKeyboardOrMouseClick(String username, String password, String role, String keyboardOrMouseClick) {
 		
-		util.doSendKeys(this.username, username);
-		util.doSendKeys(this.password, password);
+		//Clicking on Button option
+		if(keyboardOrMouseClick.length == 0) {//if nothing is specified in 4th parameter
+			//perform usual Selenium click
+			util.doClick(loginbutton);
+		}
+		else {//perform mouse or keyboard click
+			
+			if(keyboardOrMouseClick[0].trim().toLowerCase().equals("mouse")) {
+				util.mouseclickUsingAction(loginbutton);
+			}else { util.pressdownButtonUsingAction(loginbutton);}
+		}
+		
+		//Check what object to return
+		if(username == null ||username.isBlank()) {
+			//throw error if either of credentials are empty or missing
+			obj = util.getElementText(errorMessage_nullUsername);
+		}
+		else if(password == null ||password.isBlank()) {
+			obj = util.getElementText(errorMessage_nullPassword);
+		}
+		else if(username != readConfig.getUsername() && password != readConfig.getPassword()) {
+			obj = util.getElementText(errorMessage_InvalidCredentials);
+		}
+		else { //No error. Happy path
 
-		util.doClick(dropDownArraow);
-		By roleOption= By.xpath("//span[normalize-space()='"+role+"']");
-		util.doClick(roleOption);
-
-		if(keyboardOrMouseClick.trim().toLowerCase().equals("mouse")) {
-			util.mouseclickUsingAction(loginbutton);
-		}else { util.pressdownButtonUsingAction(loginbutton);}
-
-		return new HomePage(driver);
+			obj =  new HomePage(driver);
+		}
+		return obj;
+		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 }
