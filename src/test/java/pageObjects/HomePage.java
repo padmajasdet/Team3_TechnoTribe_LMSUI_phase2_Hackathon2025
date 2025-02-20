a294fcbc647288de2a423db51b44d12574a230f6
mayababuji@outlook.com <mayababuji@outlook.com>
2025-02-20 12:34:02 -0500

    added Admin should see the navigation bar text on the top right side dashboard module

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -5,6 +5,13 @@ import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.AmericanEnglish;
+import org.languagetool.rules.RuleMatch;
+
 import utilities.ElementUtil;
 
 
@@ -16,6 +23,8 @@ public class HomePage extends CommonPage {
 	private By dashboardHeader = By.xpath("//div[normalize-space()='Dashboard']");
 	private By dashboardTitile = By.xpath("//*[contains(text(),' Dashboard')]");
 	private By lmsTitleBar = By.xpath("//mat-toolbar[@class='mat-toolbar mat-primary mat-toolbar-single-row ng-star-inserted']");
+	private By lmsTitile =  By.xpath("//*[contains(text(),'LMS - Learning Management System')]");
+	private By userLabel = By.xpath("//label[@id='mat-form-field-label-1']");
 	
 	public HomePage(WebDriver driver) {
 		super(driver);
@@ -36,4 +45,42 @@ public class HomePage extends CommonPage {
 		return util.getElementText(dashboardTitile);
 		
 		}
+	public String getLMSPageTitleText() {
+		return util.getElementText(lmsTitile);
+	}
+	public int lmsTitleLocationRightNavigation() {
+		WebElement title_element = driver.findElement(lmsTitleBar);
+		// Get the X-coordinate of the element
+		int xCoordinate = title_element.getLocation().getX();
+		// Get the width of the element;
+		int width = title_element.getSize().getWidth();
+		// Calculate the right-most position
+        int rightMostLocation = xCoordinate + width;
+		return rightMostLocation;
+		
+
+	}
+	public void textSpellings() {
+		  
+		  String userText = driver.findElement(userLabel).getText();			  
+		JLanguageTool languageTool = new JLanguageTool (new AmericanEnglish());
+		  
+		  try {
+			  
+			  List<RuleMatch> errors = languageTool.check(userText);  //perform spell check on the page text
+			   if(errors.isEmpty()) {
+				   System.out.println("No spelling mistake");
+			   }else {
+				   System.out.println("Spelling mistake found");
+				   for (RuleMatch error : errors) {
+					  
+					   System.out.println("error on word:" + error.getLine() +", column" + error.getColumn() + ": " + error.getMessage());  
+				   }
+			   }
+		  }catch (IOException e) {
+			  
+			  e.printStackTrace();
+		  }		  
+	  }
+	
 }
--- src/test/java/stepDefinitions/HomePageStepDef.java
+++ src/test/java/stepDefinitions/HomePageStepDef.java
@@ -24,18 +24,20 @@ public class HomePageStepDef {
 	
 	public HomePageStepDef(TestContext Context) {
 		this.context = Context;
-		this.driver = context.getDriver();
+		driver = context.getDriver();
 		this.readConfig = new ReadConfig();
-	}
-	@When("Admin enter valid data in all field and  clicks login button")
-	public void admin_enter_valid_data_in_all_field_and_clicks_login_button() throws InterruptedException {
-		Log.logInfo("Entering valid data and clicking login");
+		homePage = new HomePage(driver);
 		loginPage = new LoginPage(driver);
-		String username = readConfig.getUsername();
-		String password = readConfig.getPassword();
-		loginPage.doLoginWithValidCredentials(username, password, "Admin");
-		
 	}
+//	@When("Admin enter valid data in all field and clicks login button")
+//	public void admin_enter_valid_data_in_all_field_and_clicks_login_button() throws InterruptedException {
+//		Log.logInfo("Entering valid data and clicking login");
+//		loginPage = new LoginPage(driver);
+//		String username = readConfig.getUsername();
+//		String password = readConfig.getPassword();
+//		loginPage.doLoginWithValidCredentials(username, password, "Admin");
+//		
+//	}
 
 	@Then("Admin should see LMS -Learning management system as title")
 	public void admin_should_see_lms_learning_management_system_as_title() {
@@ -46,7 +48,7 @@ public class HomePageStepDef {
 	}
 	@Then("LMS title should be on the top left corner of page")
 	public void lms_title_should_be_on_the_top_left_corner_of_page() {
-		homePage = new HomePage(driver);
+		
 		Point locationCordinates = homePage.lmsTitleLocation();
 		Log.logInfo("The coordinates of LMS title are "+locationCordinates);
 		int titleX = locationCordinates.getX();
@@ -60,10 +62,27 @@ public class HomePageStepDef {
 	}
 	@Then("Admin should see correct spelling in navigation bar text")
 	public void admin_should_see_correct_spelling_in_navigation_bar_text() {
-		homePage = new HomePage(driver);
+		////homePage = new HomePage(driver);
 		String dashBoardTitle = homePage.getDashboardText();
 		Log.logInfo("The Dasboard page title is "+dashBoardTitle);
 		Assert.assertTrue(dashBoardTitle.contains("Dashboard"));
+	}
+	@Then("Admin should see correct spelling and space in LMS title")
+	public void admin_should_see_correct_spelling_and_space_in_lms_title() {
+		//homePage = new HomePage(driver);
+		String expectedlmsPageTitle = "LMS - Learning Management System";
+		String actualLmsPageTitle = homePage.getLMSPageTitleText();
+		Log.logInfo("The actaul lms title is  "+actualLmsPageTitle);
+		Assert.assertEquals(actualLmsPageTitle, expectedlmsPageTitle);	}
+	
+	@Then("Admin should see the navigation bar text on the top right side")
+	public void admin_should_see_the_navigation_bar_text_on_the_top_right_side() {
+		int rightLocation = homePage.lmsTitleLocationRightNavigation();
+		Log.logInfo("The right most Location of LMS title are "+rightLocation);
+		int expectedRightMostLocation = 1470;
+		assertEquals(rightLocation,expectedRightMostLocation,"Title is not in the right corner )");
+		
+		
 	}
 	
 
