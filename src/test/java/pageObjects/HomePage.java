a22117f25b6d82e57a0311a9fd3e1b23ccec7c91
mayababuji@outlook.com <mayababuji@outlook.com>
2025-02-20 22:55:12 -0500

    added homepage features

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -21,6 +21,23 @@ public class HomePage extends CommonPage {
 	private By lmsTitile = By.xpath("//*[contains(text(),'LMS - Learning Management System')]");
 	private By navigationelement = By.xpath(
 			"//button[@class='mat-focus-indicator mat-button mat-button-base' or @class='mat-focus-indicator mat-button mat-button-base ng-star-inserted' or @class='mat-focus-indicator mat-menu-trigger mat-button mat-button-base'  ]");
+	private By pie_chart = By.xpath("//app-doughnutchart");
+	private By welcomeMessage = By.xpath("//strong[normalize-space()='Welcome sdetnumpyninja@gmail.com']");
+	private By barChartMembers = By.xpath("//mat-card-content[@class='mat-card-content']//canvas[@class='chartjs-render-monitor']");
+	private By userCount = By.xpath("//div[normalize-space()='49']");
+	private By staffCount = By.xpath("//div[normalize-space()='21']");
+	private By programCount = By.xpath("//div[normalize-space()='73']");
+	private By batchCount = By.xpath("//div[normalize-space()='59']");
+	private By staffTable = By.xpath("//mat-table[@role='grid']");
+	private By staffTablePagination = By.xpath("//div[@class='mat-paginator-range-actions']");
+	private By previousPageIcon = By.xpath("//button[@aria-label='First page']//span[@class='mat-button-wrapper']//*[name()='svg']");
+	private By firstPageIcon = By.xpath("//*[name()='path' and contains(@d,'M15.41 7.4')]");
+	
+	
+	////*[contains(text(),' Welcome sdetnumpyninja@gmail.com')]   Admin should see welcome message with user name and role
+	
+	//mat-card-content[@class='mat-card-content']  Admin should see bar chart for Active and inactive user
+////button[@aria-label='Last page']//span[@class='mat-button-wrapper']//*[name()='svg']
 
 	public HomePage(WebDriver driver) {
 		super(driver);
@@ -66,5 +83,43 @@ public class HomePage extends CommonPage {
 		return elements;
 
 	}
-
+	public boolean isPieChartDisplayed() {
+		return util.isElementDisplayed(pie_chart);
+	}
+	
+	public String welcomeMessage() {
+		return util.getElementText(welcomeMessage);
+	}
+	public boolean isBarChartMembersDisplayed() {
+		return util.isElementDisplayed(barChartMembers);
+	}
+	
+	public String userCount() {
+		return util.getElementText(userCount);
+	}
+	public String staffCount() {
+		return util.getElementText(staffCount);
+	}
+	
+	public String programCount() {
+		return util.getElementText(programCount);
+	}
+	public String batchCount() {
+		return util.getElementText(batchCount);
+	}
+	//staffTable
+	public boolean isStaffTableDisplayed() {
+		return util.isElementDisplayed(staffTable);
+	}
+	
+	public boolean isStaffTablePaginationDisplayed() {
+		return util.isElementDisplayed(staffTablePagination);
+	}
+	public boolean isPreviousPagePaginationDisabled() {
+		return util.isElementEnabled(previousPageIcon);
+	}
+	
+	public boolean isFirstPagePaginationDisabled() {
+		return util.isElementEnabled(firstPageIcon);
+	}
 }
