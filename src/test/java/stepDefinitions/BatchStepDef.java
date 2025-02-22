68e6b4ffce2852589464f43030070c6079d76d69
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2025-02-22 09:55:37 -0500

    edit scenarios for batch completed

--- src/test/java/stepDefinitions/BatchStepDef.java
+++ src/test/java/stepDefinitions/BatchStepDef.java
@@ -209,6 +209,58 @@ public class BatchStepDef  {
 		 batchPage.enterAllDetails("Save", "validAll");
 	}
 
+	@Given("Admin is on the Batch page")
+	public void admin_is_on_the_batch_page() {
+		batchPage.batchMenuClick();	
+	}
+	@When("Admin clicks the edit icon")
+	public void admin_clicks_the_edit_icon() {
+	 batchPage.clickAction("edit");	
+	}
+	@Then("Admin should see Program name value field is disabled for editing")
+	public void admin_should_see_program_name_value_field_is_disabled_for_editing() {
+		   Assert.assertEquals(batchPage.isEditBatchProgramNameEnabled(), false);	
+	}
+	@Given("Admin is on the Batch Details Page")
+	public void admin_is_on_the_batch_details_page() {
+		batchPage.batchMenuClick();	
+		 batchPage.clickAction("edit");	
+	}
+	@When("Admin edit the valid data to all the mandatory fields and click cancel button")
+	public void admin_edit_the_valid_data_to_all_the_mandatory_fields_and_click_cancel_button() {
+		 batchPage.editAllDetails("Cancel", "editAll");
+	}
+	@Then("Admin can see the batch details popup closes without editing the batch")
+	public void admin_can_see_the_batch_details_popup_closes_without_editing_the_batch() {
+		  Assert.assertEquals(batchPage.getManageBatchText(), "Manage Batch");
+	}
+	@When("Admin Updates any fields with invalid data and click save button")
+	public void admin_updates_any_fields_with_invalid_data_and_click_save_button() {
+		 batchPage.editAllDetails("Save", "editInvalid");
+	}
+	@Then("Admin should get a error message under the respective field")
+	public void admin_should_get_a_error_message_under_the_respective_field() {
+		Assert.assertEquals(batchPage.getErrorMessage(), "This field should start with an alphabet and min 2 character."); 	
+	}
+	@When("Admin edit the valid data to all the mandatory fields and click save button")
+	public void admin_edit_the_valid_data_to_all_the_mandatory_fields_and_click_save_button() {
+	    batchPage.editAllDetails("Save", "editAll");
+		
+	}
+	@Then("Admin should get a successful message for editing the batch")
+	public void admin_should_get_a_successful_message_for_editing_the_batch() {
+		Assert.assertEquals(batchPage.getToast(), "Successful");
+	}
+	@Then("Admin should see batch name value field is disabled for editing")
+	public void admin_should_see_batch_name_value_field_is_disabled_for_editing() {
+		  Assert.assertEquals(batchPage.isEditBatchNameEnabled(), false);	
+	}
+	@Then("Admin should see the Batch details pop up window in edit")
+	public void admin_should_see_the_batch_details_pop_up_window_in_edit() {
+		Assert.assertEquals(batchPage.getAddNewPageTitle(), "Batch Details");	
+	}
+
+
 	
 	
 }
--- src/test/resources/features/04_Batch.feature
+++ src/test/resources/features/04_Batch.feature
@@ -116,6 +116,38 @@ Scenario: validate close icon on the batch details pop up
 	Then batch details pop up closes
 
 #Edit icon Validation
+Scenario: Validate Edit icon feature in any row
+	Given Admin is on the Batch page
+	When Admin clicks the edit icon
+	Then Admin should see the Batch details pop up window in edit
+
+Scenario: Validate program name  value is disabled to edit
+	Given Admin is on the Batch page
+	When Admin clicks the edit icon
+	Then Admin should see Program name value field is disabled for editing
+
+Scenario: Validate batch name  value is disabled to edit
+	Given Admin is on the Batch page
+	When Admin clicks the edit icon
+	Then Admin should see batch name value field is disabled for editing
+
+Scenario: Validate editing description and No. of classes fields with invalid data in the pop up
+	Given Admin is on the Batch Details Page
+	When Admin Updates any fields with invalid data and click save button
+	Then Admin should get a error message under the respective field
+
+Scenario: validate save button in Batch details pop up
+	Given Admin is on the Batch Details Page
+	When Admin edit the valid data to all the mandatory fields and click save button 
+	Then Admin should get a successful message for editing the batch
+
+Scenario: validate cancel button in Batch details pop up
+	Given Admin is on the Batch Details Page
+	When Admin edit the valid data to all the mandatory fields and click cancel button 
+	Then Admin can see the batch details popup closes without editing the batch
+
+#Delete batch validation
+
 
 	
 		
