e948ff221227fd796979bf950501f2e97fbb7161
tohfademo <tohfademo0@gmail.com>
2025-02-24 20:02:23 -0600

    added cross browser logic and rerun test runner

--- src/test/java/testRunner/RerunTestRunner.java
+++ src/test/java/testRunner/RerunTestRunner.java
@@ -4,11 +4,12 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import utilities.ReadConfig;
 
-@CucumberOptions(features = { "src/test/resources/features/" }, 
+@CucumberOptions(features = {"@target/rerun.txt"}, 
 				glue = { "hooks", "stepDefinitions" },
 				monochrome = true, 
 				//tags="@try",
@@ -19,13 +20,13 @@ import utilities.ReadConfig;
 				//"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				/*Commenting out report for now*/		
 				 //"com.aventstack.chaintest.plugins.ChainTestCucumberListener:",
-				"rerun:target/rerun.txt"
+				"rerun:target/rerun.txt" 
 		})
 
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class RerunTestRunner extends AbstractTestNGCucumberTests {
 
 	@Override
-	@DataProvider(parallel = false)
+	@DataProvider(parallel = false) 
 	public Object[][] scenarios() {
 		return super.scenarios();
 	}
